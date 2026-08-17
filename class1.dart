d2661a06d98cc503187b00c10b209e4e89ab7ed4
DevBySarmad <devbysarmad@gmail.com>
2026-08-17 06:11:47 -0700

    Revision of Dart Language by Adding Classes of class0.dart & class1.dart & class2.dart

---
+++ class1.dart
@@ -0,0 +1,19 @@
+// class TestClass {
+//   void disp() {
+//     print("Hello World");
+//   }
+// }
+
+// void main() {
+//   TestClass c = new TestClass();
+//   c.disp();
+// }
+
+// ignore_for_file: unused_local_variable
+
+void main() {
+  int num1 = 34;
+  double num2 = 54.5;
+  bool result = num1 < num2 && num2 > num1;
+  print(result);
+}
